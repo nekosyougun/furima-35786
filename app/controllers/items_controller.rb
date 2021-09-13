e3c282060f01a61ef3f70b77ca52a6ffd8b745d9
nekosyougun <sakasitamasaru@gmail.com>
2021-09-13 22:55:49 +0900

    itemcontrollerの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :text, :introduction, :price, :category_id, :prefecture_id, :days_to_ship_id, :delivery_cost_id, :item_condition_id, :image).merge(user_id: current_user.id)
   end
 end
