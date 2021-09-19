876fd9c6e3efd06b9c1c65ccebf12dae04c95b84
nekosyougun <sakasitamasaru@gmail.com>
2021-09-19 15:44:47 +0900

    itemモデルのテストコードの実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,73 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    context '商品が出品できる時' do
+      it "全ての必須入力項目が入力されていること" do
+        expect(@item).to be_valid
+      end
+    end
+    
+    context '商品が出品できない時' do
+      it "画像が空だと登録できない" do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it "商品名が空だと登録できない" do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it "商品説明が空だと登録できない" do
+        @item.introduction = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Introduction can't be blank")
+      end
+      it "商品のカテゴリーが--だと登録できない" do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it "商品の状態が--だと登録できない" do
+        @item.item_condition_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item condition can't be blank")
+      end
+      it "配送料の負担が--だと登録できない" do
+        @item.delivery_cost_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery cost can't be blank")
+      end
+      it "発送までの日数が--だと登録できない" do
+        @item.days_to_ship_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days to ship can't be blank")
+      end
+      it "価格が空だと登録できない" do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it "価格が全角だと登録できない" do
+        @item.price = '３８９'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it "価格が文字列だと登録できない" do
+        @item.price = 'あいう'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it "userが紐づいていいないと登録できない" do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
