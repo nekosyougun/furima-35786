e95d756ead559580cab6c996c8978d299aba2574
nekosyougun <sakasitamasaru@gmail.com>
2021-09-19 15:44:01 +0900

    itemモデルの誤字修正、validatesの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,13 +4,13 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
-  belongs_to :item_conditon
+  belongs_to :item_condition
   belongs_to :prefecture
   belongs_to :delivery_cost
   belongs_to :days_to_ship
 
   with_options presence: true do
-    validates :name, :introduction, :price, :user
+    validates :name, :introduction, :price, :image
   end
   validates :price, format: { with: /\A[0-9]+\z/ }, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   validates :category_id, :prefecture_id, :item_condition_id, :delivery_cost_id, :days_to_ship_id,  numericality: { other_than: 1 , message: "can't be blank"}
