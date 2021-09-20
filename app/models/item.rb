3d865eb3acadf437fcfe336209cc1102bf625395
nekosyougun <sakasitamasaru@gmail.com>
2021-09-20 21:56:13 +0900

    Revert "バリデーション修正"
    This reverts commit f728fef12aef49c889f1d88f3f558de6b0e46323.

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,10 +10,7 @@ class Item < ApplicationRecord
   belongs_to :days_to_ship
 
   with_options presence: true do
-    validates :name
-    validates :introduction
-    validates :price
-    validates :image
+    validates :name, :introduction, :price, :image
   end
   validates :price, format: { with: /\A[0-9]+\z/ }, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   validates :category_id, :prefecture_id, :item_condition_id, :delivery_cost_id, :days_to_ship_id,  numericality: { other_than: 1 , message: "can't be blank"}
