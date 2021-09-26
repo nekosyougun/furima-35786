dc30a8d5565cb8b44e413d8a4354b2f922eead60
nekosyougun <sakasitamasaru@gmail.com>
2021-09-26 23:13:37 +0900

    orderコントローラーの追記

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,9 +5,23 @@ class OrdersController < ApplicationController
   end
 
   def new
+    @order_address = OrderAddress.new
   end
 
   def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture, :city, :house_number, :telephone_number).merge(user_id: current_user.id, item_id: item.id)
   end
 
 end
