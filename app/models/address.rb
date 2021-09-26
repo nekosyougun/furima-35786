b893533a169ae926e1377606ea4cdbffa3be6e44
nekosyougun <sakasitamasaru@gmail.com>
2021-09-26 16:19:03 +0900

    addressモデルのアソシエーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,9 @@
 class Address < ApplicationRecord
+  belongs_to :order
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
+  validates :category_id, :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+
 end
