c67d4a506745b57040ff204a1bcd113034f1934f
nekosyougun <sakasitamasaru@gmail.com>
2021-09-27 16:09:54 +0900

    order_addressモデルの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture, :city, :house_number, :telephone_number, :user_id
+  attr_accessor :postal_code, :prefecture, :city, :house_number, :telephone_number, :user_id, :item_id
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
