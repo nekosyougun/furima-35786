584c6e3d2c5623bf96b3d0c9831de3e430faa83b
nekosyougun <sakasitamasaru@gmail.com>
2021-09-30 22:52:18 +0900

    order_controller修正
    This reverts commit f68c27326c93bf71ad227180fe5bf41bb1ccadbb.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,7 +14,6 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
-      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -25,15 +24,7 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :telephone_number, :building_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :telephone_number, :building_number).merge(user_id: current_user.id, item_id: @item.id)
   end
 
-  def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-    Payjp::Charge.create(
-      amount: @item.price,
-      card: order_params[:token],
-      currency: 'jpy'
-    )
-  end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,8 +6,7 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-require("../item_price")
-require("../card")
+require("../item_price");
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
