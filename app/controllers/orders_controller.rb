5e87116f32155625ab9031ebfec13d2645d7c0a4
nekosyougun <sakasitamasaru@gmail.com>
2021-09-30 22:55:37 +0900

    order_controllerの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,6 +12,7 @@ class OrdersController < ApplicationController
 
   def create
     @item = Item.find(params[:item_id])
+    pay_item
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       @order_address.save
@@ -24,7 +25,15 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :telephone_number, :building_number).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :telephone_number, :building_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
-
+  
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
 end
