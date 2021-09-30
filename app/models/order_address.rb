8c8170428d18b9864087463f667fd77ac12fa270
nekosyougun <sakasitamasaru@gmail.com>
2021-09-30 22:56:15 +0900

    トークン情報のためのorder_addressモデル修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :telephone_number, :building_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :telephone_number, :building_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -8,6 +8,7 @@ class OrderAddress
     validates :telephone_number, format: { with: /\A\d{10,11}\z/ }
     validates :user_id
     validates :item_id
+    validates :token
   end
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
